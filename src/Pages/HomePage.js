61c8330fbff5ab906ecf06087cc9db74003b9916
Baqir Naqvi <b.naqvi99@gmail.com>
2022-08-06 01:50:41 +0500

    first commit

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -66,7 +66,7 @@ const web3Onboard = init({
   appMetadata: {
     name: 'Bootbay',
     icon: `${process.env.PUBLIC_URL}/boot-bay-assets/NFT_82x82.png`,
-    description: 'Bootbay nft minting app',
+    description: 'Bootbay nft minting application for mobile',
     recommendedInjectedWallets: [
       { name: 'MetaMask', url: 'https://metamask.io' },
       { name: 'Coinbase', url: 'https://wallet.coinbase.com/' }
